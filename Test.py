cc84e3595b2a4161360ccf1c05c90df7dcd61f48
mrmaster <mrmaster@localhost.localdomain>
2020-09-07 04:33:39 -0500

    tarea1

---
+++ Test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+from Tarea1 import *
+print ("Este programa testea todas las funciones de Tarea1.py")
+
+print ("\\nPara la funcion factorial obtenemos que 9! es: ", factorial (9))
+
+print ("\nPara los coeficientes binomiales tenemos que si n=4, k=2 el coeficiente binomial es: ",binomial(4,2))
+
+print ("\nAhora el triangulo de pascal con 6 filas: \n")
+pascal(6)
+
+print ("\nPara la moneda, obtenemos que si la lanzamos 100 veces")
+monedaA()
+monedaB()
+
+
+# In[ ]:
+
+
+
+
